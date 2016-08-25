a1f60e98d57bd08d7f97b63aeaa9aa6d7d8d2023
Marios Hadjiantonis <mhadjiantonis@gmail.com>
2016-08-25 00:39:01 -0400

    Move network task in a background thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -35,10 +36,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        new DownloadEarthquakeData().execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -54,4 +52,28 @@ public class MainActivity extends AppCompatActivity {
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
+
+    /**
+     * Downloads details for an earthquake in a background thread
+     */
+    private class DownloadEarthquakeData extends AsyncTask<String, Void, Event> {
+
+        @Override
+        protected Event doInBackground(String... strings) {
+            if (strings.length < 1 || strings[0].isEmpty() || strings[0] == null) {
+                return null;
+            } else {
+                return Utils.fetchEarthquakeData(strings[0]);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            if (event == null) {
+                return;
+            }
+            updateUi(event);
+        }
+    }
+
 }
